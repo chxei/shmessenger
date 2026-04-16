5bdbc5fd3dd010987f03cb386fdadbad82df6493
chxei <chxei5667@gmail.com>
2026-04-16 05:25:24 +0400

    fix: change CountryRepository ID type from Integer to String

--- src/main/java/org/chxei/shmessenger/repository/user/CountryRepository.java
+++ src/main/java/org/chxei/shmessenger/repository/user/CountryRepository.java
@@ -3,5 +3,5 @@ package org.chxei.shmessenger.repository.user;
 import org.chxei.shmessenger.entity.user.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CountryRepository extends JpaRepository<Country, Integer> {
+public interface CountryRepository extends JpaRepository<Country, String> {
 }
